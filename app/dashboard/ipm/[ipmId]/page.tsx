689ddaf3e4a38e3c6bf7a9e18fd24c53e3f6a245
Ni Nyoman Sri Malini <easyttd@gmail.com>
2026-03-08 02:19:34 +0800

    hapus dummy data

--- app/dashboard/ipm/[ipmId]/page.tsx
+++ app/dashboard/ipm/[ipmId]/page.tsx
@@ -1,6 +1,5 @@
 import PageContainer from "@/components/layout/page-container";
 import { Card, CardHeader } from "@/components/ui/card";
-import { AlatData } from "@/features/alat/alat";
 import type { Alat } from "@/lib/definitions/tipe-alat";
 
 type PageProps = {
@@ -11,23 +10,23 @@ export default async function IpmPage({ params }: PageProps) {
 
   const { ipmId } = await params;
 
-  const getAlatById = async (id: string): Promise<Alat> => {
-    const alat = AlatData.find((alat) => alat.id === parseInt(id));
-    if (!alat) {
-      throw new Error(`Alat dengan id ${id} tidak ditemukan`);
+  // const getAlatById = async (id: string): Promise<Alat> => {
+  //   const alat = AlatData.find((alat) => alat.id === parseInt(id));
+  //   if (!alat) {
+  //     throw new Error(`Alat dengan id ${id} tidak ditemukan`);
 
-    }
-    return alat;
-  };
+  //   }
+  //   return alat;
+  // };
 
-  const alat = await getAlatById(ipmId);
+  // const alat = await getAlatById(ipmId);
 
 
   return (
     <PageContainer
       pageTitle={`Detail Alat`}>
       <Card>
-        <CardHeader>Detail Alat {alat.nama}</CardHeader>
+        {/* <CardHeader>Detail Alat {alat.nama}</CardHeader> */}
       </Card>
     </PageContainer>
   );
