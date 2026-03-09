52e1da184417664e9bb9c0e94f57ca809f36b491
Ni Nyoman Sri Malini <easyttd@gmail.com>
2026-03-09 10:01:13 +0800

    update proxy

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 import { NextResponse } from 'next/server';
 
-const isAdminRoute = createRouteMatcher(['/admin(.*)'])
+const isAdminRoute = createRouteMatcher(['/dashboard(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isAdminRoute(req) && (await auth()).sessionClaims?.metadata?.role !== 'admin') {
